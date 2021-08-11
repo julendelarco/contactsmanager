a05ebe7236fc45ba922f2191dfe6b2f5cc045786
Julen Del Arco Campos <julen.delarco@alumni.mondragon.edu>
2021-08-11 11:26:29 +0200

    maven initializr created

---
+++ src/test/java/com/itechis/contactsmanager/ContactsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechis.contactsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
